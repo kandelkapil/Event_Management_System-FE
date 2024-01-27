48b09632cd51bad69fb230219cca3ad2c5f4d410
Kapil Kandel <kapil@evolveasia.co>
2024-01-27 16:39:18 +0545

    api routes defined

---
+++ src/constants/apiRoutes.ts
@@ -0,0 +1,8 @@
+export const apiRoutes = {
+  refreshToken: "/auth/refresh",
+  signUp: "/auth/signup",
+  login: "/auth/login",
+  logOut: "/auth/logout",
+  imageUpload: "/upload/image",
+  user:'/user'
+};
