514262c8dcae439c013b0dcc2b1be6b6be9ae5f7
Kapil Kandel <kapil@evolveasia.co>
2024-01-28 15:46:00 +0545

    protected routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,41 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Dashbaoard from "#views/Dashboard";
-import PrivateRoute from "#views/PrivateRoute/PrivateRoute";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Dashboard from "#views/Dashboard";
 import UserProfile from "#views/UserProfile";
 import SignUp from "#views/SignUp";
 import Login from "#views/Login";
 import Events from "#views/Events";
+import NavBar from "#components/NavBar";
+import CreateEvents from "#views/Events/Views/EventForm";
+import EventDetails from "#views/Events/Views/EventDetails";
+import { useAuth } from "#hooks/useAuthHook";
 
 const App = () => {
+  const { user } = useAuth();
+
   return (
     <Router>
+      <NavBar />
       <Routes>
-        <Route path="/" element={<Dashbaoard />} />
-        <Route path="/signup" element={<SignUp />} />
+        {!user?.userId && <Route path="*" element={<Navigate to="/login" />} />}
+        {user?.userId && (
+          <>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/signup" element={<SignUp />} />
+
+            {/* Render protected routes only when the user is logged in */}
+            <Route path="/events/*" element={<Events />} />
+            <Route path="/events/create" element={<CreateEvents />} />
+            <Route path="/events/edit/:id" element={<CreateEvents />} />
+            <Route path="/events/:id" element={<EventDetails />} />
+            <Route path="/profile" element={<UserProfile />} />
+          </>
+        )}
         <Route path="/login" element={<Login />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/profile" element={<UserProfile />} />
-
-        {/* <Route
-          element={
-            <PrivateRoute
-              allowedRoles={[ROLES.ROLE_MODERATOR, ROLES.ROLE_ADMIN]}
-            />
-          }
-        >
-          <Route path="/user-list" element={<UserList />} />
-        </Route> */}
-
-        {/* <Route
-          element={
-            <PrivateRoute
-              allowedRoles={[
-                ROLES.ROLE_MODERATOR,
-                ROLES.ROLE_ADMIN,
-                ROLES.ROLE_USER,
-              ]}
-            />
-          }
-        >
-          <Route path="/profile" element={<UserProfile />} />
-        </Route> */}
-
-        {/* <Route path="/unauthorized" element={<Unauthorized />} /> */}
-        <Route path="*" element={<h1>Error page</h1>} />
       </Routes>
     </Router>
   );
