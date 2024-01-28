b01df5eb2645a050939c1042b9a048db176b4804
Kapil Kandel <kapil@evolveasia.co>
2024-01-28 15:56:34 +0545

    changes in event list

--- src/views/Events/index.tsx
+++ src/views/Events/index.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
-import useEventsControllers from './controllers/useEventsControllers';
+import { useNavigate } from "react-router-dom";
+import { EventListContainer } from "./EventList.Styled";
+import EventCard from "./components/EventCard";
+import { EventCardContainer } from "./components/EventCard.Styled";
+import useEventsListControllers from "./controllers/useEventsListControllers";
+import { ToastContainer } from "react-toastify";
 
-const Events = () => {
-    const {} = useEventsControllers();
+const EventList = () => {
+  const { events, eventDeleteHandler, eventRegisterHandler } =
+    useEventsListControllers();
+  const navigate = useNavigate();
+
   return (
-    <div>index</div>
-  )
-}
+    <EventListContainer>
+      <ToastContainer autoClose={3000} newestOnTop />
+      <div className="wrapper">
+        <button
+          className="create-event"
+          onClick={() => navigate("/events/create")}
+        >
+          Create Event
+        </button>
+        <div className="events-container">
+          {events?.map((event) => (
+            <EventCard
+              picture={event?.picture}
+              venue_time={event?.venue_time}
+              name={event?.name}
+              attendees={event?.attendees}
+              id={event?.id}
+              createdBy={event?.created_by}
+              eventDeleteHandler={eventDeleteHandler}
+              eventRegisterHandler={eventRegisterHandler}
+            />
+          ))}
+        </div>
+      </div>
+    </EventListContainer>
+  );
+};
 
-export default Events
+export default EventList;
